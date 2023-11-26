2bdb51ee9d638929a936fcf5a510b0e36596ab30
alxm <am@alexandermoreno.com>
2023-11-26 11:25:21 -0600

    adding public assets

---
+++ public/js/basic_player.js
@@ -0,0 +1,29 @@
+const possiblefiles = ["rave_digger.mp3", "running_out.mp3", "80s_vibe.mp3"]
+
+const playlist = ["80s_vibe.mp3"]
+
+
+const audioPlayer = document.querySelector("#audioPlayer");
+
+async function dequeueAndPlayNext() {
+  if (playlist.length > 0) {
+    const filename = playlist.shift()
+    audioPlayer.src = `/audio/${filename}`;
+    await audioPlayer.play();
+  }
+}
+
+audioPlayer.addEventListener('ended', function() {
+  console.log("Song ended");
+  dequeueAndPlayNext();
+})
+
+
+
+setInterval(() => {
+  playlist.push(possiblefiles[Math.floor(Math.random() % (2))])
+}, 4000);
+
+(function () { 
+
+dequeueAndPlayNext().then(() => console.log("Started playback queue"_) });)()
